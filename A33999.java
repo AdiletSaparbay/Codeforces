61c5f1fcb30b368ee9efcd5b01fb74b0b66ec096
AdiletSaparbay <49226275+AdiletSaparbay@users.noreply.github.com>
2020-06-23 18:42:09 +0600

    Add files via upload

---
+++ A33999.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A33999 {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static void solve(){
+        FastReader in = new FastReader();
+        String s[] = in.nextLine().split("\\+");
+        Arrays.sort(s);
+        for (int i = 0; i < s.length - 1; i++) {
+            System.out.print(s[i] + "+");
+        }
+        System.out.print(s[s.length - 1]);
+
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
